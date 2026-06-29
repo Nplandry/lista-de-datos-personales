2e1b1e278464a68c942a5f24943471ce0978ceed
Nicopobletee <149529377+Nplandry@users.noreply.github.com>
2026-06-29 01:34:04 -0400

    Add zoneless change detection provider to tests

--- frontend/src/app/components/persona-lista/persona-lista.spec.ts
+++ frontend/src/app/components/persona-lista/persona-lista.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 import { PersonaLista } from './persona-lista';
 
@@ -9,6 +10,7 @@ describe('PersonaLista', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PersonaLista],
+      providers: [provideZonelessChangeDetection()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PersonaLista);
